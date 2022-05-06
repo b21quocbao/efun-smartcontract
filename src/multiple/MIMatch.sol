3d66dcb1bfad61371b884775ae8dfc4a9ff5feb2
Tuan Tran <mrtrantuan@yahoo.it>
2022-05-06 11:38:37 +0700

    v1

---
+++ src/multiple/MIMatch.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity =0.8.4;
+
+import "./MDataTypes.sol";
+pragma experimental ABIEncoderV2;
+
+interface MIMatch {
+    function info(uint256 _matchId)
+        external
+        view
+        returns (MDataTypes.Match memory _match);
+}
